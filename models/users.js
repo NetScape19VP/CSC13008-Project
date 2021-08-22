6fd842181677c0f12c95fd7aa2557c093605da72
andrena <huenhu0512@gmail.com>
2021-08-22 16:14:36 +0700

    change `recentBoard` to `boards`

--- models/users.js
+++ models/users.js
@@ -5,13 +5,9 @@ const usersSchema = new mongoose.Schema({
     userId: String,
     name: String,
     avtURL: String,
-<<<<<<< HEAD
-    boards: [{type:String}]
-=======
-    recentBoard: [{
+    boards: [{
         type: mongoose.Schema.ObjectId
     }]
->>>>>>> 400665df679b1ee0475e1ddb50d4a9698c960efa
 })
 
 module.exports = mongoose.model('users', usersSchema)
