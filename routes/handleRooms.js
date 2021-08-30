873dbc12072f91c36b63b1d68b2e26600b9d8d4a
andrena <huenhu0512@gmail.com>
2021-08-30 19:31:52 +0700

    handle room and redirect

--- routes/handleRooms.js
+++ routes/handleRooms.js
@@ -15,24 +15,36 @@ const createRoom = (req, res) => {
 	});
 }
 
-const checkRoomExist = (req, res) => {
-	const check = await checkRoom(req.params.roomID);
-	
-	async function checkRoom(roomID) {
+const isExistRoom = async (req, res) => {
+	await function checkRoom(roomID) {
 		room.findOne({ _id: roomID }).then((room) => {
 			if (room) {
-				console.log('Found room with ID: ' + req.params.roomID);
+				console.log('Found room with ID: ' + roomID);
 				return true;
 			}
-			console.log('Cannot find room with ID: ' + req.params.roomID);
+			console.log('Cannot find room with ID: ' + roomID);
 			return false;
 		});
 	}
 
+	const check = checkRoom(req.params.roomID);
 	return check;
+}
+
+const deleteRoom = (req, res) => {
+	room.findOneAndRemove({
+		_id: req.params.roomID
+	}).then((room) => {
+		if (room === null) {
+			console.log('Can not delete room with ID: ' + req.params.roomID);
+		} else {
+			console.log('Delete successful room with ID: ' + req.params.roomID);
+		}
+	})
 }
 
 module.exports = {
 	createRoom,
-	checkRoomExist
+	isExistRoom,
+	deleteRoom
 }
