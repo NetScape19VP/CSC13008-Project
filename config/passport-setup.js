f1d9196229024347aa6b24b945a4e379659e7f93
phatnguqqq-2428 <74006956+phatnm-student-hcmus@users.noreply.github.com>
2021-09-01 13:17:54 +0700

    heroku

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -20,7 +20,7 @@ passport.use(
         //option for gg strategy
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret,
-        callbackURL: `http://localhost:3000/auth/google/callback`
+        callbackURL: `https://co-op-whiteboard.herokuapp.com/auth/google/callback`
 
     }, async (accessToken, refreshToken, profile, done) => {
         //passport callback func
